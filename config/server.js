d157ba37214a24a7cff28f6c46bce401d4446e33
Amjad Iqbal Khan <amjad.iqbal@VIBD01.local>
2021-08-26 14:12:28 +0500

    'Initial'

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3a0596da95f1de2e71bff853019f23b9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '28f25df3-12dd-496a-8848-553125689ae3'
+};
